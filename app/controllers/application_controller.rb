95aa6a720d9f7647ff477e44c58e4abfcb16a053
Takeru Yamanaka <Takeru@takeru-no-MacBook-Pro-Retina.local>
2019-09-08 11:52:44 +0900

    [finish]5-2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+   
+     before_action :configure_permitted_parameters, if:  :devise_controller?
+    #  アクション（devise利用の機能）が実行される前に :configure_permitted_parametersが実行される
+
+     protected
+    # privateのようなポジションで使われるやつ
+    # 
+
+    def configure_permitted_parameters 
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    end
+
 end
